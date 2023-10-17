277925900a428d40683203e57f70c0131f5f67e2
Matt Turco <matt.turco@gmail.com>
2023-10-16 22:31:10 -0700

    Update root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -38,7 +38,7 @@ export default function App() {
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta name="viewport" content="width=device-width,initial-scale=1.0" />
         <link
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🗄️</text></svg>"
